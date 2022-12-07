12b05f399ec19871c40ab90530bd87bfaab8ad67
Renars Miljons <reku.miljons@gmail.com>
2022-12-08 01:04:52 +0200

    update and delete user handlers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,17 +1,37 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/usersModel");
+const mongoose = require("mongoose");
 
-// creaating a token for authentication
+// creating a token for authentication
 const createToken = (_id) => {
     return jwt.sign({_id}, process.env.JWT_SECRET, { expiresIn: "999 years" });
 }
 
 const getUsers = async (req, res) => {
-
+    try {
+        const users = await User.find({});
+        res.status(200).json({ data: users });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
 };
 
 const getUser = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: "Invalid ID" });
+    }
+
+    try {
+        const user = await User.findOne({_id: id});
+        if (!user) {
+            return res.status(404).json({ error: "No user found" });
+        }
+        res.status(200).json({ data: user });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
 };
 
 const signupUser = async (req, res) => {
@@ -39,11 +59,46 @@ const loginUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: "Invalid ID" });
+    }
+
+    // TODO: add ability to change password
+    if (req.body.password) {
+        return res.status(400).json({ error: "Can't update password" });
+    }
+
+    // if username is empty string
+    if (!req.body.username) {
+        return res.status(400).json({ error: "Invalid username" });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate({ _id: id }, { ...req.body });
+        if (!user) {
+            return res.status(404).json({ error: "No user found" });
+        }
+        res.status(200).json({ data: user });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 const deleteUser = async (req, res) => {
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: "Invalid ID" });
+    }
+
+    try {
+        const user = await User.findByIdAndDelete({ _id: id });
+        res.status(200).json({ data: user });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 module.exports = {
